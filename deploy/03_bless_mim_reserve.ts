ce6a365e447c7d64f1897373747c3b7a55447474
plinki <geolazaria@gmail.com>
2022-02-15 02:35:30 +0200

    send mim to deployer after bless

--- deploy/03_bless_mim_reserve.ts
+++ deploy/03_bless_mim_reserve.ts
@@ -7,6 +7,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await getNamedAccounts();
 
   const mimAddr = '0x82f0B8B456c1A451378467398982d4834b6829c1';
+  const mim = await hre.ethers.getContractAt('IERC20', mimAddr);
 
   const king = await hre.ethers.getContractAt('King', (await deployments.get('King')).address);
   const oracle = await hre.ethers.getContractAt(
@@ -15,7 +16,22 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   );
 
   await (await king.bless(mimAddr, 1000, 2000, 5, oracle.address, false, true, 5000)).wait();
+
+  if (!hre.network.live) {
+    const addressWithMim = '0xad8F72A7612Bb91B2dfaB09E54464aaA5150914E';
+    const balanceToSend = hre.ethers.utils.parseEther('500000');
+    await hre.network.provider.request({
+      method: 'hardhat_impersonateAccount',
+      params: [addressWithMim],
+    });
+
+    const mimHolder = await hre.ethers.getSigner(addressWithMim);
+
+    await mim.connect(mimHolder).transfer(deployer, balanceToSend);
+  }
+
+
 };
 export default func;
 func.dependencies = ['FUSD', 'King', 'KingReserveMIMOracle'];
-func.tags = ["BlessMimReserve"]
+func.tags = ['BlessMimReserve']
